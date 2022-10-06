aa18473a3a45fd4bc5ac2145fe3955120c595ecd
Spencer Long <spencerlong0624@gmail.com>
2022-10-06 05:26:20 -0400

    updated password for dummy document reviewers

---
+++ docs-core/src/main/resources/db/update/dbupdate-030-0.sql
@@ -0,0 +1,5 @@
+update T_USER set USE_PASSWORD_C = '$2y$10$xg0EEKVUehutDI1m6qQhVeFz7SMQMl1jQzjf2KkVsR2c7aV2vyyjK' where USE_ID_C = 'spencer';
+update T_USER set USE_PASSWORD_C = '$2y$10$xg0EEKVUehutDI1m6qQhVeFz7SMQMl1jQzjf2KkVsR2c7aV2vyyjK' where USE_ID_C = 'hanna';
+update T_USER set USE_PASSWORD_C = '$2y$10$xg0EEKVUehutDI1m6qQhVeFz7SMQMl1jQzjf2KkVsR2c7aV2vyyjK' where USE_ID_C = 'jennifer';
+update T_USER set USE_PASSWORD_C = '$2y$10$xg0EEKVUehutDI1m6qQhVeFz7SMQMl1jQzjf2KkVsR2c7aV2vyyjK' where USE_ID_C = 'anesha';
+update T_CONFIG set CFG_VALUE_C = '30' where CFG_ID_C = 'DB_VERSION';
